86a4d67f995b52ae8d793be07a33214610af47ba
Shane <143796207+Shane0821@users.noreply.github.com>
2026-01-27 20:38:06 +0800

    feat: replace void trait

---
+++ include/shcoro/stackless/traits.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <iostream>
+#include <tuple>
+
+namespace shcoro {
+
+struct empty {
+    // for debugging
+    friend std::ostream& operator<<(std::ostream& os, const empty&) {
+        return os << "[empty]";
+    }
+};
+
+template <typename T>
+struct void_to_empty {
+    using type = T;
+};
+
+template <>
+struct void_to_empty<void> {
+    using type = empty;
+};
+
+template <typename... Args>
+struct replace_void {
+    using type = std::tuple<typename void_to_empty<Args>::type...>;
+};
+
+template <typename... Args>
+using replace_void_t = typename replace_void<Args...>::type;
+
+};  // namespace shcoro
