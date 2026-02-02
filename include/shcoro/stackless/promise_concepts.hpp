14ec1f2494616afdb7f918ee75ddf47eec93be13
Shane <143796207+Shane0821@users.noreply.github.com>
2026-02-02 22:43:51 +0800

    refacto: upd getter return value

--- include/shcoro/stackless/promise_concepts.hpp
+++ include/shcoro/stackless/promise_concepts.hpp
@@ -8,9 +8,9 @@ namespace shcoro {
 
 // Async promise should store AsyncScheduler
 template <typename Promise>
-concept PromiseSchedulerConcept = requires(Promise p, AsyncScheduler sched) {
+concept PromiseSchedulerConcept = requires(Promise p, Scheduler sched) {
     // Must have getter
-    { p.get_scheduler() } -> std::same_as<AsyncScheduler>;
+    { p.get_scheduler() } -> std::same_as<Scheduler&>;
 
     // Must have setter
     p.set_scheduler(sched);
