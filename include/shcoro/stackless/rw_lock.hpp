8e6993a7d75a44ace06a175aae2277d45979ea09
Shane <143796207+Shane0821@users.noreply.github.com>
2026-02-06 20:29:21 +0800

    feat: fair rw lock

--- include/shcoro/stackless/rw_lock.hpp
+++ include/shcoro/stackless/rw_lock.hpp
@@ -8,18 +8,22 @@
 #include "noncopyable.h"
 
 namespace shcoro {
+
+enum class RWLockPolicy { READ_RPIOR, FAIR };
+
+template <RWLockPolicy Policy = RWLockPolicy::READ_RPIOR>
 class RWLock final : noncopyable {
    public:
     struct ReadAwaiter {
         ReadAwaiter(RWLock* lock) : lock_(lock) {}
 
-        bool await_ready() noexcept { return !lock_->has_writer_; }
+        bool await_ready() noexcept { return !lock_->writer_active_; }
 
         void await_suspend(std::coroutine_handle<> caller) {
             lock_->waiting_list_.push(caller);
         }
 
-        void await_resume() noexcept { lock_->read_cnt_++; }
+        void await_resume() noexcept { lock_->active_reader_++; }
 
         RWLock* lock_{nullptr};
     };
@@ -28,14 +32,14 @@ class RWLock final : noncopyable {
         WriteAwaiter(RWLock* lock) : lock_(lock) {}
 
         bool await_ready() noexcept {
-            return !(lock_->read_cnt_ > 0 || lock_->has_writer_);
+            return (lock_->active_reader_ == 0 && !lock_->writer_active_);
         }
 
         void await_suspend(std::coroutine_handle<> caller) {
             lock_->waiting_list_.push(caller);
         }
 
-        void await_resume() noexcept { lock_->has_writer_ = true; }
+        void await_resume() noexcept { lock_->writer_active_ = true; }
 
         RWLock* lock_{nullptr};
     };
@@ -45,17 +49,17 @@ class RWLock final : noncopyable {
     ~RWLock() = default;
 
     bool try_read_lock() noexcept {
-        if (has_writer_) {
+        if (writer_active_) {
             return false;
         }
-        read_cnt_++;
+        active_reader_++;
         return true;
     }
     bool try_write_lock() noexcept {
-        if (read_cnt_ || has_writer_) {
+        if (active_reader_ || writer_active_) {
             return false;
         }
-        has_writer_ = true;
+        writer_active_ = true;
         return false;
     }
 
@@ -63,15 +67,15 @@ class RWLock final : noncopyable {
     [[nodiscard]] WriteAwaiter write_lock() { return WriteAwaiter(this); }
 
     void read_unlock() {
-        read_cnt_--;
-        if (read_cnt_ == 0 && !waiting_list_.empty()) {
+        active_reader_--;
+        if (!waiting_list_.empty() && active_reader_ == 0) {
             auto nxt = waiting_list_.front();
             waiting_list_.pop();
             nxt.resume();
         }
     }
     void write_unlock() {
-        has_writer_ = false;
+        writer_active_ = false;
         if (!waiting_list_.empty()) {
             auto nxt = waiting_list_.front();
             waiting_list_.pop();
@@ -81,7 +85,92 @@ class RWLock final : noncopyable {
 
    private:
     std::queue<std::coroutine_handle<>> waiting_list_;
-    size_t read_cnt_{0};
-    bool has_writer_{false};
+    size_t active_reader_{0};
+    bool writer_active_{false};
+};
+
+template <>
+class RWLock<RWLockPolicy::FAIR> final : noncopyable {
+   public:
+    struct ReadAwaiter {
+        ReadAwaiter(RWLock* lock) : lock_(lock) {}
+
+        bool await_ready() noexcept {
+            return (!lock_->writer_active_ && !lock_->waiting_writer_);
+        }
+
+        void await_suspend(std::coroutine_handle<> caller) {
+            lock_->waiting_list_.push(caller);
+        }
+
+        void await_resume() noexcept { lock_->active_reader_++; }
+
+        RWLock* lock_{nullptr};
+    };
+
+    struct WriteAwaiter {
+        WriteAwaiter(RWLock* lock) : lock_(lock) {}
+
+        bool await_ready() noexcept {
+            return (!lock_->active_reader_ && !lock_->writer_active_);
+        }
+
+        void await_suspend(std::coroutine_handle<> caller) {
+            lock_->waiting_list_.push(caller);
+            lock_->waiting_writer_++;
+        }
+
+        void await_resume() noexcept {
+            lock_->waiting_writer_--;
+            lock_->writer_active_ = true;
+        }
+
+        RWLock* lock_{nullptr};
+    };
+
+    RWLock() = default;
+    RWLock(RWLock&& other) noexcept = default;
+    ~RWLock() = default;
+
+    bool try_read_lock() noexcept {
+        if (writer_active_ || waiting_writer_) {
+            return false;
+        }
+        active_reader_++;
+        return true;
+    }
+    bool try_write_lock() noexcept {
+        if (active_reader_ || writer_active_) {
+            return false;
+        }
+        writer_active_ = true;
+        return false;
+    }
+
+    [[nodiscard]] ReadAwaiter read_lock() { return ReadAwaiter(this); }
+    [[nodiscard]] WriteAwaiter write_lock() { return WriteAwaiter(this); }
+
+    void read_unlock() {
+        active_reader_--;
+        if (!waiting_list_.empty() && active_reader_ == 0) {
+            auto nxt = waiting_list_.front();
+            waiting_list_.pop();
+            nxt.resume();
+        }
+    }
+    void write_unlock() {
+        writer_active_ = false;
+        if (!waiting_list_.empty()) {
+            auto nxt = waiting_list_.front();
+            waiting_list_.pop();
+            nxt.resume();
+        }
+    }
+
+   private:
+    std::queue<std::coroutine_handle<>> waiting_list_;
+    size_t active_reader_{0};
+    size_t waiting_writer_{0};
+    bool writer_active_{false};
 };
 }  // namespace shcoro
