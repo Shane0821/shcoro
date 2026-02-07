a1efa40a5d2ab3038f15c53b7e268218356e8c01
Shane <143796207+Shane0821@users.noreply.github.com>
2026-02-07 18:41:16 +0800

    feat: hashmap based task indexing

--- include/shcoro/stackless/timer.hpp
+++ include/shcoro/stackless/timer.hpp
@@ -18,19 +18,19 @@ class TimedScheduler {
 
     void register_coro(std::coroutine_handle<> coro, time_t duration) {
         SHCORO_LOG("timer register: ", duration);
-        coros_.insert(
+        auto [it, suc] = coros_.insert(
             {std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()) +
                  duration,
              coro});
+        coro_map_[coro.address()] = it;
     }
 
     void unregister_coro(std::coroutine_handle<> coro) {
-        for (auto it = coros_.begin(); it != coros_.end(); it++) {
-            if (it->second == coro) {
-                SHCORO_LOG("timer unregister");
-                coros_.erase(it);
-                break;
-            }
+        auto addr = coro.address();
+        if (coro_map_.find(addr) != coro_map_.end()) {
+            SHCORO_LOG("timer unregister");
+            coros_.erase(coro_map_[addr]);
+            coro_map_.erase(addr);
         }
     }
 
@@ -53,6 +53,7 @@ class TimedScheduler {
 
    private:
     std::set<std::pair<time_t, std::coroutine_handle<>>> coros_;
+    std::unordered_map<void*, decltype(coros_)::iterator> coro_map_;
 };
 
 struct TimedAwaiter {
