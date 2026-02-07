8ce51444e5188f19c01aa94583d381dc9792db04
Shane <143796207+Shane0821@users.noreply.github.com>
2026-02-07 17:01:53 +0800

    feat: support value registration and no value registration

--- include/shcoro/stackless/scheduler.hpp
+++ include/shcoro/stackless/scheduler.hpp
@@ -9,14 +9,56 @@
 
 namespace shcoro {
 
+template <class SchedulerT>
+concept SchedulerNoValue = (!requires { typename SchedulerT::value_type; }) &&
+                           requires(SchedulerT& sched, std::coroutine_handle<> h) {
+                               // A "no-value" scheduler only needs to be able to
+                               // register/unregister a coroutine.
+                               sched.register_coro(h);
+                               sched.unregister_coro(h);
+                           };
+
+template <class SchedulerT>
+concept SchedulerWithValue = requires(SchedulerT& sched, std::coroutine_handle<> h,
+                                      typename SchedulerT::value_type v,
+                                      const typename SchedulerT::value_type& cv) {
+    typename SchedulerT::value_type;
+
+    // accept either const-ref or rvalue (or both)
+    requires(
+        requires { sched.register_coro(h, cv); } ||
+        requires { sched.register_coro(h, std::move(v)); });
+
+    sched.unregister_coro(h);
+};
+
+template <class SchedulerT>
+concept SchedulerConcept = SchedulerNoValue<SchedulerT> || SchedulerWithValue<SchedulerT>;
+
 class Scheduler {
+   private:
+    struct SchedulerBase;
+
+    template <SchedulerNoValue SchedulerT>
+    struct NonOwningSchedulerModelNoValue;
+
+    template <SchedulerWithValue SchedulerT>
+    struct NonOwningSchedulerModelWithValue;
+
    public:
     Scheduler() = default;
 
-    template <class SchedulerT>
-        requires(!std::is_same_v<SchedulerT, Scheduler>)
+    template <SchedulerConcept SchedulerT>
     Scheduler(SchedulerT& sched)
-        : pimpl_(std::make_unique<NonOwningSchedulerModel<SchedulerT>>(sched)) {}
+        : pimpl_([&]() -> std::unique_ptr<SchedulerBase> {
+              if constexpr (SchedulerWithValue<SchedulerT>) {
+                  return std::make_unique<NonOwningSchedulerModelWithValue<SchedulerT>>(
+                      sched);
+              } else {
+                  return std::make_unique<NonOwningSchedulerModelNoValue<SchedulerT>>(
+                      sched);
+              }
+          }()) {}
 
     Scheduler(const Scheduler& other)
         : pimpl_(other.pimpl_ ? other.pimpl_->clone() : nullptr) {}
@@ -32,6 +74,10 @@ class Scheduler {
 
     operator bool() const noexcept { return pimpl_ != nullptr; }
 
+    friend void scheduler_register_coro(Scheduler& sched, std::coroutine_handle<> h) {
+        sched.pimpl_->register_coro(h);
+    }
+
     template <class ValueT>
     friend void scheduler_register_coro(Scheduler& sched, std::coroutine_handle<> h,
                                         ValueT&& v) {
@@ -45,14 +91,38 @@ class Scheduler {
    private:
     struct SchedulerBase {
         virtual ~SchedulerBase() = default;
+        virtual void register_coro(std::coroutine_handle<>) = 0;
         virtual void register_coro(std::coroutine_handle<>, const void*) = 0;
         virtual void unregister_coro(std::coroutine_handle<>) = 0;
         virtual std::unique_ptr<SchedulerBase> clone() const = 0;
     };
 
-    template <class SchedulerT>
-    struct NonOwningSchedulerModel final : SchedulerBase {
-        explicit NonOwningSchedulerModel(SchedulerT& s) : sched_(&s) {}
+    template <SchedulerNoValue SchedulerT>
+    struct NonOwningSchedulerModelNoValue final : SchedulerBase {
+        explicit NonOwningSchedulerModelNoValue(SchedulerT& s) : sched_(&s) {}
+
+        void register_coro(std::coroutine_handle<> h) override {
+            sched_->register_coro(h);
+        }
+
+        void register_coro(std::coroutine_handle<> h, const void*) override {}
+
+        void unregister_coro(std::coroutine_handle<> h) override {
+            sched_->unregister_coro(h);
+        }
+
+        std::unique_ptr<SchedulerBase> clone() const override {
+            return std::make_unique<NonOwningSchedulerModelNoValue>(*this);
+        }
+
+        SchedulerT* sched_{nullptr};
+    };
+
+    template <SchedulerWithValue SchedulerT>
+    struct NonOwningSchedulerModelWithValue final : SchedulerBase {
+        explicit NonOwningSchedulerModelWithValue(SchedulerT& s) : sched_(&s) {}
+
+        void register_coro(std::coroutine_handle<> h) override {}
 
         void register_coro(std::coroutine_handle<> h, const void* value) override {
             using ValueT = typename SchedulerT::value_type;
@@ -64,7 +134,7 @@ class Scheduler {
         }
 
         std::unique_ptr<SchedulerBase> clone() const override {
-            return std::make_unique<NonOwningSchedulerModel>(*this);
+            return std::make_unique<NonOwningSchedulerModelWithValue>(*this);
         }
 
         SchedulerT* sched_{nullptr};
